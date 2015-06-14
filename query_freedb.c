641c346b327d6c2c91d2c4f01a0c7d5304264b09
Bryan Ames <b130610@gmail.com>
2015-06-14 15:21:58 -0500

    added the two database query programs

---
+++ query_freedb.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <cddb/cddb.h>
+
+cddb_conn_t *init_conn();
+cddb_disc_t *init_disc();
+void read_db(cddb_conn_t *conn, cddb_disc_t *disc);
+int search_db(cddb_conn_t *conn, cddb_disc_t *disc, const char *str);
+void lowercase(char *str);
+
+int main(int argc, char *argv[]) {
+	/*initialize the connection to the freedb server */
+	cddb_conn_t *conn = NULL;
+	cddb_disc_t *disc;
+	conn = init_conn();
+
+	/* set track info */
+	if (argc != 3) {
+		fprintf(stderr, "usage: cddb_query <discid> <category>\n");
+		return -1;
+	}
+	char *p;
+	unsigned long discid = strtoul(argv[1], &p, 16);
+	char *cat = argv[2];
+	lowercase(cat);
+
+	/* initialize the disc */
+	disc = init_disc();
+	cddb_disc_set_discid(disc, discid);
+	cddb_disc_set_category_str(disc, cat);
+
+	/* query the database to populate the disc info */
+	read_db(conn, disc);
+
+	/* format an output track info */
+	const char *artist = cddb_disc_get_artist(disc);
+	const char *album = cddb_disc_get_title(disc);
+	unsigned int date = cddb_disc_get_year(disc);
+	const char *label = "";
+	const char *genre = cddb_disc_get_genre(disc);
+	printf("%s\t%s\t%d\t%s\t%s\t", artist, album, date, label, genre);
+
+	return 0;
+}
+
+/* initialize connection to cddb server */
+cddb_conn_t *init_conn() {
+	cddb_conn_t *conn;
+	if ((conn = cddb_new()) == NULL) {
+		fprintf(stderr, "out of memory, "
+	                "unable to create connection structure");
+	}
+
+	/* HTTP settings */
+	cddb_http_enable(conn);
+	cddb_set_server_port(conn, 80);
+	cddb_set_server_name(conn, "freedb.org");
+	cddb_set_http_path_query(conn, "/~cddb/cddb.cgi");
+	cddb_set_http_path_submit(conn, "/~cddb/submit.cgi");
+
+	return conn;
+}
+
+/* initialize disc */
+cddb_disc_t *init_disc() {
+	cddb_disc_t *disc;
+	if ((disc = cddb_disc_new()) == NULL) {
+	    fprintf(stderr, "out of memory, unable to create disc");
+	    exit(-1);
+	}
+
+	return disc;
+}
+
+/* read data from cddb database */
+void read_db(cddb_conn_t *conn, cddb_disc_t *disc) {
+	int success = cddb_read(conn, disc);
+	if (!success) {
+	    /* something went wrong, print error */
+	    cddb_error_print(cddb_errno(conn));
+	    exit(-1);
+	}
+}
+
+/* change all characters in a string to lowercase (for cddb_read call in main) */
+void lowercase(char *str) {
+	for (int i = 0; str[i] != '\0'; i++) {
+		tolower(str[i]);
+		str[i] = tolower(str[i]);
+	}
+}
---
+++ query_musicbrainz.py
@@ -0,0 +1,124 @@
+import sys
+import codecs
+import musicbrainz2.webservice as ws
+import CDDB
+
+UTF8Writer = codecs.getwriter('utf8')
+sys.stdout = UTF8Writer(sys.stdout)
+
+UTF8Reader = codecs.getreader('utf8')
+sys.stdin = UTF8Reader(sys.stdin)
+
+fields = ['artist', 'album', 'date', 'label', 'genre', 'cddb']
+
+artist = ""
+album = ""
+date = ""
+
+def setArtist(name=None):
+	global artist
+	artist = u"artist:" + name
+	return 1
+
+def setAlbum(name=None):
+	global album
+	album = u"release:" + name
+	return 1
+
+def setDate(num=None):
+	global date
+	date = u"date:" + str(num)
+	return 1
+
+options = {
+	   "-a":setArtist,
+	   "-t":setAlbum,
+	   "-d":setDate
+	  }
+
+def bestTag(el):
+	maxScore = 0
+	best = None
+	for entity in el:
+		if entity.count > 0:
+			maxScore = entity.count
+			best = entity
+	return best
+
+def search(artist=None, album=None, date=None):
+	query = ws.Query()
+#	searchTerms = ws.ReleaseFilter(title=album, artistName=artist, query=date)
+	searchTerms = ws.ReleaseFilter(query=artist + ", " + album + ", " + date)
+	try:
+		results = query.getReleases(searchTerms)
+	except:
+		sys.exit(2)
+	releaseInclude = ws.ReleaseIncludes(artist=True,
+	                                    counts=True,
+	                                    releaseEvents=True,
+	                                    discs=True, 
+	                                    tracks=True, 
+	                                    artistRelations=True, 
+	                                    releaseRelations=True, 
+	                                    trackRelations=True, 
+	                                    urlRelations=True, 
+	                                    labels=True, 
+	                                    tags=True, 
+	                                    isrcs=True, 
+	                                    releaseGroup=True)
+	try:
+		release = query.getReleaseById(results[0].release.id, releaseInclude)
+	except:
+		sys.exit(2)
+#	release = None
+
+	# todo: make sure to choose the correct releaseEvent rather than just the first
+	try:
+		Id = release.id
+	except AttributeError:
+		Id = None
+	try:
+		Title = release.title
+	except:
+		Title = None
+	try:
+		Artist = release.artist.name
+	except AttributeError:
+		Artist = None
+	try:
+		Date = release.releaseEvents[0].date
+	except AttributeError:
+		Date = None
+	except IndexError:
+		Date = None
+	try:
+		Label = release.releaseEvents[0].label.name
+	except AttributeError:
+		Label = None
+	except IndexError:
+		Label = None
+	try:
+		Genre = bestTag(release.tags)
+	except AttributeError:
+		Genre = None
+	return {
+		"id":Id,
+		"album":Title,
+		"artist":Artist,
+		"date":Date,
+		"label":Label,
+		"genre":Genre
+	}
+
+for i in range(len(sys.argv)):
+	if sys.argv[i] in options:
+		options[sys.argv[i]](unicode(sys.argv[i+1], 'utf-8'))
+
+md = search(artist, album, date)
+
+#print "%s\t%s\t%s\t%s\t%s\t\t" % (data['artist'], data['album'], data['date'], data['label'], data['genre'])
+for f in fields:
+	try:
+		print md[f], '\t',
+	except KeyError:
+		print '', '\t',
