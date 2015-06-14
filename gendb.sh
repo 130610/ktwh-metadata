d01f0e8894e58cd942280379de3f84e8c9b0d039
Bryan Ames <b130610@gmail.com>
2015-06-14 15:22:29 -0500

    added wrapper script gendb.sh which depends on all three query programs

---
+++ gendb.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+function output_md {
+	IFS=' '
+	md=$(python2 query_metadata.py "$1")
+	mb_md=$(python2 query_musicbrainz.py -a \""$(echo "$md" | cut -f1)"\" -t \""$(echo "$md" | cut -f2)"\" -d \""$(echo "$md" | cut -f3)"\")
+	fdb_md=$(exec ./query_freedb $(echo "$md" | cut -f6) $(echo "$md" | cut -f5))
+	echo "$md$mb_md$fdb_md"
+}
+
+function get_files {
+	shopt -s nocasematch
+
+	for file in $(find "$1"); do
+		if [[ "$file" =~ .*\.flac$ ]] || [[ "$file" =~ .*\.mp3$ ]] ; then
+			echo "$file"
+		fi
+	done
+}
+
+
+files=$(get_files "$1")
+#get_files "$1"
+
+for file in $files; do
+	while : ;do
+		md=$(output_md "$file")
+		[[ $? == 2 ]] || break
+		echo "--- 503 Error: denial of service, waiting for 5 second ---" 1>&2 && sleep 5
+	done
+	echo "$file	$md"
+done
