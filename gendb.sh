656a5b66af12a3f48233d22328c4d9e22ba735b4
Bryan Ames <b130610@gmail.com>
2015-07-01 12:23:09 -0500

    hid error messages from python (I catch the ones I'm interested in for now)

--- gendb.sh
+++ gendb.sh
@@ -5,7 +5,7 @@ IFS=$(echo -en "\n\b")
 
 function output_md {
 	IFS=' '
-	md=$(python2 query_metadata.py "$1")
+	md=$(python2 query_metadata.py "$1" 2>/dev/null)
 	mb_md=$(python2 query_musicbrainz.py -n "$(echo "$md" | cut -f2)" -a \""$(echo "$md" | cut -f3)"\" -t \""$(echo "$md" | cut -f4)"\" -d \""$(echo "$md" | cut -f5)"\")
 	ret=$?
 	fdb_md=$(exec timeout .2 ./query_freedb $(echo "$md" | cut -f8) $(echo "$md" | cut -f7) $(echo "$md" | cut -f2))
